2d42de0e864602315cb7da1fe03f681245a16126
Patrick Schumacher <hello@thepoddi.com>
2018-07-06 10:56:15 +0200

    plugin base

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# kirby-robots
-Robots.txt Plugin for Kirby CMS
+# Kirby Plugin: Robots
+
+robots.txt for kirby websites.
+
+## Installation
+
+### Kirby CLI
+`kirby plugin:install thepoddi/kirby-robots`
+
+### Git
+Include this repository as a submodule `git submodule add https://github.com/thepoddi/kirby-robots.git site/plugins/kirby-robots` or copy it manually to `/site/plugins/`. *Attention: Plugin directory must named like the plugin file (kirby-robots).*
+
+
+## Usage
+This plugin sets a robots file to `/robots.txt` as a kirby route. There is no actual file generated.
+
+
+## Config
+
+The robots file can be configured via Kirby’s config file `/site/config/config.php`.
+
+### Ignore Pages
+Ignore pages by uid. (array) *Default: error*
+```
+c::set( 'robots.ignore.pages', array('error') );
+```
+
+Ignore pages by intended templates. (array) *Default: error*
+```
+c::set( 'robots.ignore.templates', array('error') );
+```
+
+Ignore invisible pages. (boolean) *Default: true*
+```
+c::set( 'robots.ignore.invisible', true );
+```
+
+### Set Sitemap File
+Set sitemap file in robots.txt. (string) *Default: sitemap.xml*
+```
+c::set( 'robots.sitemap', 'sitemap.xml' );
+```
+
+## Authors
+
+**Patrick Schumacher** - [GitHub](https://github.com/thepoddi) [Website](https://www.thepoddi.com)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ kirby-robots.php
@@ -0,0 +1,43 @@
+<?php
+/** KIRBY PLUGIN: Robots
+ * -------------------------------------------------------------------
+ * Plugin Name: Robots
+ * Description: robots.txt for Kirby Websites.
+ * @version    1.0.0
+ * @author     Patrick Schumacher <hello@thepoddi.com>
+ * @link       https://github.com/ThePoddi/kirby-robots
+ * @license    MIT
+ */
+
+$ignorePages      = c::get( 'robots.ignore.pages', array('error') );
+$ignoreTemplates  = c::get( 'robots.ignore.templates', array('error') );
+$ignoreInvisible  = c::get( 'robots.ignore.invisible', true );
+$sitemap          = c::get( 'robots.sitemap', 'sitemap.xml' );
+
+// ROBOTS.txt
+kirby()->routes(
+  array(
+    array(
+      'pattern' => 'robots.txt',
+      'method' => 'GET',
+      'action' => function() use ( $ignorePages, $ignoreTemplates, $ignoreInvisible, $sitemap ) {
+        $robots = '# robots.txt for ' . site()->url() . "\n";
+        $robots .= 'User-agent: *' . "\n";
+
+        // disallow crawling for some pages
+        foreach( site()->index() as $p ) :
+          if (
+            in_array( $p->uid(), $ignorePages ) || // ignore pages defined in config
+            in_array( $p->intendedTemplate(), $ignoreTemplates ) || // ignore templates defined in config
+            ( $ignoreInvisible === true && $p->isInvisible() ) // ignore invisible pages
+          ) $robots .= 'Disallow: /' . $p->uid() . "\n";
+        endforeach;
+
+        // sitemap location
+        $robots .= 'Sitemap: ' . site()->url() . '/' . $sitemap . "\n";
+
+        return new Response( $robots, 'text' );
+      }
+    )
+  )
+);
